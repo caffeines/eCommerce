76f0e80f5cc8715c8d4b30a0b5972bbc74216c47
caffeines <me.caffeines@gmail.com>
2019-03-12 17:38:32 +0600

    login__page done

---
+++ application/components/Button.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<button type="button" class="btn btn-primary">Primary</button>
+	</div>
+</template>
+<script>
+	export default {};
+</script>
+<style>
+</style>
---
+++ application/nuxt.config.js
@@ -0,0 +1,70 @@
+const pkg = require("./package");
+
+module.exports = {
+  mode: "universal",
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Lato:100,300,400,400i,700"
+      }
+    ],
+    script: []
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
+
+  /*
+   ** Global CSS
+   */
+  css: [
+    "@/assets/css/style.css",
+    "@/assets/bootstrap/bootstrap.css",
+    "@/assets/css/ionicons.min.css"
+  ],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa"
+  ],
+  /*
+   ** Axios module configuration
+   */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+};
