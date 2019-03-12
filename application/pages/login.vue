f2167c179d3d9836e0c5f051abb9ea594adf0754
caffeines <me.caffeines@gmail.com>
2019-03-13 04:33:18 +0600

    minor bug fix & animatios.scss add

--- application/pages/login.vue
+++ application/pages/login.vue
@@ -1,36 +1,39 @@
 <template>
-	<div class="row">
-		<div class="col-sm-4"></div>
-		<div class="col-sm-4">
-			<div class="ccard">
-				<div class="ccard__side">
-					<form action="#" class="form">
-						<div class="mb-2 mt-1">
-							<h2 class="heading-secondary">gogonjo</h2>
-						</div>
-						<div class="form__group">
-							<input type="email" class="form__input" placeholder="Email address" id="email" required>
-							<label for="email" class="form__lebel form__lebel-mail">Eamil address</label>
-						</div>
-						<div class="form__group">
-							<input type="password" class="form__input" placeholder="Password" id="password" required>
-							<label for="password" class="form__lebel form__lebel-pass">Password</label>
-						</div>
+	<div>
+		<navbar></navbar>
+		<div class="row">
+			<div class="col-sm-4"></div>
+			<div class="col-sm-4">
+				<div class="ccard">
+					<div class="ccard__side">
+						<form action="#" class="form">
+							<div class="mb-2 mt-1">
+								<h2 class="heading-secondary">gogonjo</h2>
+							</div>
+							<div class="form__group">
+								<input type="email" class="form__input" placeholder="Email address" id="email" required>
+								<label for="email" class="form__lebel form__lebel-mail">Eamil address</label>
+							</div>
+							<div class="form__group">
+								<input type="password" class="form__input" placeholder="Password" id="password" required>
+								<label for="password" class="form__lebel form__lebel-pass">Password</label>
+							</div>
 
-						<div class="form__group u-margin-top-big">
-							<button class="cbtn cbtn-primary">Sign in</button>
+							<div class="form__group u-margin-top-big">
+								<button class="cbtn cbtn-primary">Sign in</button>
+							</div>
+							<div class="forgot text-center">
+								<a href="#" class="cbtn-text">Forgot Password &#63;</a>
+							</div>
+						</form>
+						<div class="via__social">
+							<i class="cbtn cbtn__icon-white ion-logo-facebook ion-logo"></i>
+							<i class="cbtn cbtn__icon-white ion-logo-twitter ion-logo"></i>
+							<i class="cbtn cbtn__icon-white ion-logo-google ion-logo"></i>
 						</div>
-						<div class="forgot text-center">
-							<a href="#" class="cbtn-text">Forgot Password &#63;</a>
+						<div class="text-center">
+							<a href="#" class="cbtn-text">Sign up &rarr;</a>
 						</div>
-					</form>
-					<div class="via__social">
-						<i class="cbtn cbtn__icon-white ion-logo-facebook ion-logo"></i>
-						<i class="cbtn cbtn__icon-white ion-logo-twitter ion-logo"></i>
-						<i class="cbtn cbtn__icon-white ion-logo-google ion-logo"></i>
-					</div>
-					<div class="text-center">
-						<a href="#" class="cbtn-text">Sign up &rarr;</a>
 					</div>
 				</div>
 			</div>
@@ -39,7 +42,12 @@
 </template>
 
 <script>
-	export default {};
+	import navBar from "../components/navBar.vue";
+	export default {
+		components: {
+			navbar: navBar
+		}
+	};
 </script>
 
 
