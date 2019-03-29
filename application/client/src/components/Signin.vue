357a382c8f36c135902aa52cc72a147e6b9f79c7
caffeines <me.caffeines@gmail.com>
2019-03-29 07:08:54 +0600

    signin components

---
+++ application/client/src/components/Signin.vue
@@ -0,0 +1,8 @@
+<template>
+	<div>
+		<h2>Welcome back!</h2>
+	</div>
+</template>
+<script>
+	export default {};
+</script>
---
+++ application/client/src/views/Signin.vue
@@ -0,0 +1,11 @@
+<template>
+	<Signin/>
+</template>
+<script>
+	import Signin from "@/components/Signin";
+	export default {
+		components: {
+			Signin
+		}
+	};
+</script>
