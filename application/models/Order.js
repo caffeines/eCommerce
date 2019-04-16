646d584b2423e2e3cabc8d95e90ecc102421be93
caffeines <me.caffeines@gmail.com>
2019-04-16 10:25:05 +0600

    address form & order schema

--- application/models/Order.js
+++ application/models/Order.js
@@ -1,18 +1,70 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
+const orderSchema = new mongoose.Schema({
   products: [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product"
+      productItem: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+      },
+      quantity: {
+        type: Number,
+        required: true
+      },
+      color: {
+        type: String,
+        required: true
+      },
+      size: {
+        type: String,
+        required: true
+      }
     }
   ],
+  shippingType: {
+    type: String,
+    required: true
+  },
+  address: {
+    addressLine: {
+      type: String,
+      required: true
+    },
+    fullName: {
+      type: String,
+      required: true
+    },
+    contact: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    zip: {
+      type: String,
+      required: true
+    },
+    country: {
+      type: String,
+      required: true
+    }
+  },
   consumer: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User"
   },
   consumerEmail: {
     type: String,
+    required: true
+  },
+  total: {
+    type: Number,
     required: true
   }
 });
