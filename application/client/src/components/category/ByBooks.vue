7d66a3e6c4a3c7ec9fa370970997b58781fe4f5b
caffeines <me.caffeines@gmail.com>
2019-04-28 04:32:21 +0600

    minor update

--- application/client/src/components/category/ByBooks.vue
+++ application/client/src/components/category/ByBooks.vue
@@ -1,77 +1,88 @@
 <template>
-	<v-container grid-list-xl>
-		<v-layout wrap>
-			<v-flex
-				mt-4
-				xs12
-				xl2
-				lg3
-				md4
-				sm6
-				v-for="product in productsByCategory"
-				:key="'product'"
-				@click="viewProduct(product._id)"
-			>
-				<v-hover>
-					<v-card
-						height="400"
-						width="260"
-						class="card mx-auto"
-						slot-scope="{ hover }"
-						color="grey lighten-4"
+	<div>
+		<div v-if="!fflag && productsByCategory <= 0" class="_icon">
+			<span class="same">4</span>
+			<v-icon x-large color="red">sentiment_dissatisfied</v-icon>
+			<span class="same">4</span>
+			<div class="found">Nothing found</div>
+		</div>
+		<div v-else>
+			<v-container grid-list-xl>
+				<v-layout wrap>
+					<v-flex
+						mt-4
+						xs12
+						xl2
+						lg3
+						md4
+						sm6
+						v-for="product in productsByCategory"
+						:key="'product'"
+						@click="viewProduct(product._id)"
 					>
-						<v-img :src="product.picture[0]" :lazy-src="product.picture[0]" aspect-ratio="1">
-							<v-expand-transition>
-								<div
-									v-if="hover"
-									class="d-flex transition-fast-in-fast-out black darken-3 v-card--reveal display-3 white--text"
-									style="height: 100%;"
-								>view</div>
-							</v-expand-transition>
-						</v-img>
+						<v-hover>
+							<v-card
+								height="400"
+								width="260"
+								class="card mx-auto"
+								slot-scope="{ hover }"
+								color="grey lighten-4"
+							>
+								<v-img :src="product.picture[0]" :lazy-src="product.picture[0]" aspect-ratio="1">
+									<v-expand-transition>
+										<div
+											v-if="hover"
+											class="d-flex transition-fast-in-fast-out black darken-3 v-card--reveal display-3 white--text"
+											style="height: 100%;"
+										>view</div>
+									</v-expand-transition>
+								</v-img>
 
-						<v-card-title>
-							<div>
-								<h3 class="headline price">&#2547;&nbsp;{{product.price}}</h3>
-								<div class="text">
-									<strong>{{ product.productName }}</strong>
+								<v-card-title>
+									<div>
+										<h3 class="headline price">&#2547;&nbsp;{{product.price}}</h3>
+										<div class="text">
+											<strong>{{ product.productName }}</strong>
+										</div>
+										<h4>
+											<strong>{{product.createdBy.shopName}}</strong>
+										</h4>
+									</div>
+								</v-card-title>
+								<div class="ml-3">
+									<v-layout row>
+										<v-rating
+											v-model="product.rating.rate"
+											color="yellow darken-3"
+											background-color="grey darken-1"
+											empty-icon="$vuetify.icons.ratingFull"
+											half-increments
+											dense
+											readonly
+										></v-rating>
+										<span class="rating">{{product.rating.rate}} ({{product.rating.totalNumberOfRating}})</span>
+									</v-layout>
 								</div>
-								<h4>
-									<strong>{{product.createdBy.shopName}}</strong>
-								</h4>
-							</div>
-						</v-card-title>
-						<div class="ml-3">
-							<v-layout row>
-								<v-rating
-									v-model="product.rating.rate"
-									color="yellow darken-3"
-									background-color="grey darken-1"
-									empty-icon="$vuetify.icons.ratingFull"
-									half-increments
-									dense
-									readonly
-								></v-rating>
-								<span class="rating">{{product.rating.rate}} ({{product.rating.totalNumberOfRating}})</span>
-							</v-layout>
-						</div>
-					</v-card>
-				</v-hover>
-			</v-flex>
-		</v-layout>
-	</v-container>
+							</v-card>
+						</v-hover>
+					</v-flex>
+				</v-layout>
+			</v-container>
+		</div>
+	</div>
 </template><script>
 	import { mapGetters } from "vuex";
 	export default {
 		data() {
 			return {
+				fflag: true,
 				rating: 0,
 				card_text: "Vue t-Shirt",
 				flag: false,
 				productsByCategory: []
 			};
 		},
-		created() {
+		mounted() {
 			this.getProducts();
 		},
 		computed: {
@@ -96,6 +107,7 @@
 						this.productsByCategory.push(this.products[i]);
 					}
 				}
+				this.fflag = false;
 			}
 		}
 	};
@@ -117,6 +129,24 @@
 	.text {
 		color: rgb(114, 113, 113);
 		font-size: 13px;
+	}
+	.same {
+		font-size: 40px;
+		margin: 0 2px 0 2px;
+		font-weight: 400;
+	}
+	.found {
+		display: block;
+		color: #f34643bd;
+		font-size: 3rem;
+		font-weight: 700;
+		text-align: center;
+	}
+	._icon {
+		display: block;
+		color: #f34643 !important;
+		text-align: center;
+		margin: 200px auto;
 	}
 	.rating {
 		color: #f9a825;
